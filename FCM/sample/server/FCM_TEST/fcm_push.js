144b83db55ffbbfb4ed4607855cf3a89083c4075
정인권 <jeong-inkwon@inkwon.local>
2021-01-04 19:14:28 +0900

    로컬 서버 ip 주소 변경

--- FCM/sample/server/FCM_TEST/fcm_push.js
+++ FCM/sample/server/FCM_TEST/fcm_push.js
@@ -1,5 +1,5 @@
 const request = require('request');
-request('http://192.168.0.97:3000/push', function (error, response, body) {
+request('http://192.168.170.39:3000/push', function (error, response, body) {
      if (!error && response.statusCode == 200) {
        console.log(body) // Show the HTML for the Google homepage. 
      }
